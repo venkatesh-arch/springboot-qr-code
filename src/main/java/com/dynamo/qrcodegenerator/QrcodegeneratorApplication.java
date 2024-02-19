f39e2eb29f58cade465475bca6b415fdebfd517a
venkatesh <thurumerla.venkatesh@gmail.com>
2024-02-20 04:26:34 +0530

    Code changes push

---
+++ src/main/java/com/dynamo/qrcodegenerator/QrcodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.dynamo.qrcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodegeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dynamo/qrcodegenerator/QrcodegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamo.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
